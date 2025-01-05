daf659ae6d579acf1cf82e7189e20b501bd84412
Lukas Anell <lukasmanell@gmail.com>
2025-01-04 17:13:09 -0800

    Keep Multiplying Found Values by Two

---
+++ Python/2101-2200/2154.py
@@ -0,0 +1,8 @@
+from typing import List
+
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        while original in nums:
+            original *= 2
+        return original
