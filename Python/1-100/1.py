285a9738e283854e89eb456262f6b2014294067b
Lukas Anell <lukasmanell@gmail.com>
2025-01-18 13:02:49 -0800

    Swap LC 1 & 2, Update LC 26

--- Python/1-100/1.py
+++ Python/1-100/1.py
@@ -1,26 +1,10 @@
-# Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
-
-from typing import Optional
+from typing import List
 
 
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        x = ListNode()
-        nextX = x
-        carry = 0
-        while l1 or l2 or carry:
-            if l1:
-                carry += l1.val
-                l1 = l1.next
-            if l2:
-                carry += l2.val
-                l2 = l2.next
-            nextX.next = ListNode(carry % 10)
-            nextX = nextX.next
-            carry //= 10
-        return x.next
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length = len(nums)
+        for i in range(length):
+            for j in range(length):
+                if nums[i] + nums[j] == target and i != j:
+                    return [i, j]
--- Python/1-100/2.py
+++ Python/1-100/2.py
@@ -1,10 +1,26 @@
-from typing import List
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+from typing import Optional
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        length = len(nums)
-        for i in range(length):
-            for j in range(length):
-                if nums[i] + nums[j] == target and i != j:
-                    return [i, j]
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        x = ListNode()
+        nextX = x
+        carry = 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            nextX.next = ListNode(carry % 10)
+            nextX = nextX.next
+            carry //= 10
+        return x.next
